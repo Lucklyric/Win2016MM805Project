31fc5bfff27b570e3aa9cad3e9044b46fb6885e3
Lucklyric <xinyao1@ualberta.ca>
2016-04-21 00:12:01 -0600

    1. Finalize the real case

--- scripts/realCase.m
+++ scripts/realCase.m
@@ -1,53 +1,46 @@
-clear;
-
+clear;close;
+%% The parameters of the camera we used in this project
 g_f = 6.1;
 dx = 7.44/3648;
 dy = 5.58/2736;
 g_fx = g_f/dx;
 g_fy = g_f/dy;
-%% Roll 180 Using the same image with init pose and roll 180 pose
-pointTracker = vision.PointTracker('MaxBidirectionalError', 10);
-
+% Pre-set parameters of pan and roll angles in degree
+pan_angle = 9;
+til_angle = 3;
+%% Read Real Image from files and
 imgInitR = imread('../images/new/init.JPG');
-imgPanR = imread('../images/new/p5.JPG');
-imgPanT = imread('../images/new/t5.JPG');
-
+imgPanR = imread('../images/new/p9.JPG');
+imgTilR = imread('../images/new/t6.JPG');
+imgRoll180R = imread('../images/new/r180.JPG');
+%% Parse them to the grayscale image
 imgInit = rgb2gray(imgInitR);
+imgFakeR180 = flip(flip(imgInit,1),2);
 imgPan = rgb2gray(imgPanR);
-imgTil = rgb2gray(imgPanT);
+imgTil = rgb2gray(imgTilR);
 
-figure,
-imshow(imgInit);
-rectInit = getrect;
+imgRoll180 = rgb2gray(imgRoll180R);
+[h,w] = size(imgInit);
+%% Return Init Image countour
+[x_bar,y_bar] = returnContour(imgInit,'Init');
 
-maskInit = imgInit(floor(rectInit(2)):floor(rectInit(2)+rectInit(4)),floor(rectInit(1)):floor(rectInit(1)+rectInit(3)));
-R = im2bw(maskInit,graythresh(maskInit));
-[r,c] = ind2sub(size(R),find(R==0));
+% %% Return Fake 180 Image countour and compare the idea image center
+% [x_fakeR180_bar,y_fakeR180_bar] = returnContour(imgFakeR180,'Fake_Roll_180');
+% 
+% delta_x = (x_bar+x_fakeR180_bar)/2;
+% delta_y = (y_bar+y_fakeR180_bar)/2;
+% fprintf('Ideal Image Center:cx:%.2f,cy:%.2f\n',w/2,h/2);
+% fprintf('Fake Roll 180:delta_x:%.2f,delta_y:%.2f\n',delta_x,delta_y);
 
-y_bar = mean(r)+rectInit(2);
-x_bar = mean(c)+rectInit(1);
+%% Return pan 5 Image countour
+[x_pan_bar,y_pan_bar] = returnContour(imgPan,'Pan');
 
+%% Return Til 5 Image countour
+[x_til_bar,y_til_bar] = returnContour(imgTil,'Til');
 
+%% Return roll 180 Image countour
+[x_roll180_bar,y_roll180_bar] = returnContour(imgFakeR180,'roll_180');
 
-figure,
-imshow(imgPan);
-rectPan = getrect;
-maskPan = imgPan(floor(rectPan(2)):floor(rectPan(2)+rectPan(4)),floor(rectPan(1)):floor(rectPan(1)+rectPan(3)));
-panR = im2bw(maskPan,graythresh(maskPan));
-[r,c] = ind2sub(size(panR),find(panR==0));
+%% Apply StrategyC
+[Cfx,Cfy,Cdx,Cdy] = strategyC(x_bar,x_pan_bar,pan_angle*pi/180,y_bar,y_til_bar,til_angle*pi/180,180*pi/180,x_roll180_bar,y_roll180_bar); 
 
-y_bar_p = mean(r)+rectPan(2);
-x_bar_p = mean(c)+rectPan(1);
-
-figure,
-imshow(imgTil);
-rectTil = getrect;
-maskTil = imgTil(floor(rectTil(2)):floor(rectTil(2)+rectTil(4)),floor(rectTil(1)):floor(rectTil(1)+rectTil(3)));
-tilR = im2bw(maskTil,graythresh(maskTil));
-[r,c] = ind2sub(size(tilR),find(tilR==0));
-
-y_bar_t = mean(r)+rectTil(2);
-x_bar_t = mean(c)+rectTil(1);
-
-
-[Cfx,Cfy] = strategyC(x_bar,x_bar^2,x_bar_p,8*pi/180,y_bar,y_bar^2,y_bar_t,4*pi/180); 
